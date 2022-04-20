bdae8e2fdbec10bc9acd7b06f73d00a65a0a8496
misha <dedovmu@gmail.com>
2022-04-20 21:22:49 +0300

    Added clear function Drawing is now done via mouse drag TODO Grid density fucntions "Brushes"

--- script.js
+++ script.js
@@ -1,19 +1,59 @@
-function createGrid() {
+let mouseDown = 0;
+document.body.onmousedown = () => (mouseDown = 1);
+document.body.onmouseup = () => (mouseDown = 0);
+
+let gridSize = 260;
+let gridState = "default"
+
+function createGrid(gridSize, gridState) {
     const gridContainer = document.querySelector(".gridContainer");
     // should be i < 256
-    for (let i = 0; i < 260; i++) {
+    for (let i = 0; i < gridSize; i++) {
         const gridItem = document.createElement("div");
-        gridItem.className = "gridItem";
+        if (gridState === "default") {
+            gridItem.className = "gridItem";
+        }
+        else if (gridState === "small") {
+            gridItem.className = "gridItemSmall";
+        }
+        gridItem.addEventListener("mouseenter", draw)
         gridContainer.appendChild(gridItem);
-        draw(gridItem);
     }   
 }
-function draw(gridItem) {
-    gridItem.addEventListener("mouseenter", function( event ) {
-        event.target.style.backgroundColor = "yellow";
-        console.log("wow")
+
+function draw(evnt) {
+    if (evnt.type === "mouseenter" && !mouseDown) return
+    else {
+        evnt.target.style.backgroundColor = "yellow";
+    }
+}
+
+function clear() {
+    const clearButton = document.querySelector("#clear");
+    clearButton.addEventListener("click", () => {
+        for (let i = 0; i < gridSize; i++) {
+            const gridContainer = document.querySelector(".gridContainer");
+            const gridItem = document.querySelector(".gridItem")
+            console.log("jwgkjb") 
+            gridContainer.removeChild(gridItem);
+            }
+        createGrid(gridSize, gridState);
     });
 }
 
+function smallGrid() {
+    const smallButton = document.querySelector("#smallGrid");
+    smallButton.addEventListener("click", () => {
+        gridSize = 500;
+        gridState = "small";
+        console.log("wow")
+        clear();
+        createGrid(gridSize, gridState);
+    });
+}
 
-createGrid();
+window.onload = () => {
+    createGrid(gridSize, gridState);
+    clear();
+    smallGrid();
+  }
